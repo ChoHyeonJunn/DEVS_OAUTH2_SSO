95f63c8ca660bbf1d748f8605c4c91ad50fabf7e
chohyeonjunn <ancsbbc@naver.com>
2020-06-09 02:52:00 +0900

    20200609

--- backup_code/code_20200510/sso-server/src/main/java/com/devs/sso/server/service/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devs.sso.server.service;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.devs.sso.server.domain.Client;
-
-public interface ClientRepository extends CrudRepository<Client, String> {
-	//
-}
--- backup_code/code_20200510_2/sso-server/src/main/java/com/devs/sso/server/model/domain/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devs.sso.server.model.domain.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.devs.sso.server.model.domain.entity.Client;
-
-public interface ClientRepository extends CrudRepository<Client, String> {
-	//
-}
